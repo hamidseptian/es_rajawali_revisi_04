22179d21fcba423095b549757106c5eb861abd41
hamidseptian <hamidseptian@gmail.com>
2021-08-29 15:38:32 +0700

    first commit

---
+++ assets/koneksi.php
@@ -0,0 +1,20 @@
+<?php 
+//  $conn = mysqli_connect('sql305.epizy.com','epiz_29266118','BGJXzFWgJMknt','epiz_29266118_es_rajawali');
+   $conn = mysqli_connect('localhost','root','','es_rajawali');
+  date_default_timezone_set('Asia/Jakarta');
+  // error_reporting(0);
+  $namabulan = array(
+                '01' => 'Januari',
+                '02' => 'Februari',
+                '03' => 'Maret',
+                '04' => 'April',
+                '05' => 'Mei',
+                '06' => 'Juni',
+                '07' => 'Juli',
+                '08' => 'Agustus',
+                '09' => 'September',
+                '10' => 'Oktober',
+                '11' => 'November',
+                '12' => 'Desember',
+        );
+ ?>
